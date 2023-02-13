7967bc0858dc2a61f408050fe3c5d41ef44a5eac
SIEGWARDi <mhm.142521@hotmail.com>
2023-02-14 00:56:31 +0300

    selection sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,5 +1,28 @@
+import java.lang.reflect.Array;
+
 public class Sorter {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] array = {1,3,42,5,1,5,66,2,234,1,0};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("---------------------------");
+        selectionSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
+    public static void selectionSort(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++){  
+            int index = i;   
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+    }  
 }
